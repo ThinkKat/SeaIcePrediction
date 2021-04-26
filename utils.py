eaee8f49c78f9bbd9eb98f972684021ba3e4d17e
unknown <jwy06048@gmail.com>
2021-04-27 00:23:19 +0900

    Modify the read file method

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-def dataGenerartor(path, X, y = None, X_dt =None, y_dt=None, BATCH_SIZE = 8, transpose = False):
+def dataGenerartor(path, X, y = None, X_dt =None, y_dt=None, BATCH_SIZE = 8, transpose = False, usePath = False):
     X_batch_data = []
     y_batch_data = []
     i = 0
@@ -9,7 +9,7 @@ def dataGenerartor(path, X, y = None, X_dt =None, y_dt=None, BATCH_SIZE = 8, tra
         y = [None] * len(X)
 
     for (features, label) in zip(X, y):
-        obs =  obs_generator(path, features, label, transpose = transpose)
+        obs =  obs_generator(path, features, label, transpose = transpose, usePath=usePath)
         X_data = obs[0]
         y_data = obs[1]
         if X_dt is not None:
@@ -25,10 +25,18 @@ def dataGenerartor(path, X, y = None, X_dt =None, y_dt=None, BATCH_SIZE = 8, tra
             X_batch_data = []
             y_batch_data = []
 
-def obs_generator(path, features, label=None, transpose=False):
+def obs_generator(path, features, label=None, transpose=False, usePath=False):
+    '''
+
+    usePath: using os.path.join (if False, faster than using usePath, but you have to check the file path)
+    '''
+    
     featuresArr = []
     for feature in features:
-        feature_path = os.sep.join(path, feature)
+        if usePath:
+            feature_path = os.path.join(path, feature)
+        else:
+            feature_path = path + feature
         featureArr = np.load(feature_path)
         # transpose
         if transpose:
@@ -39,7 +47,10 @@ def obs_generator(path, features, label=None, transpose=False):
     featuresArr = np.array(featuresArr)
 
     if label is not None:
-        label_path = os.sep.join(path, label)
+        if usePath:
+            label_path = os.path.join(path, label)
+        else:
+            label_path = path + feature
         labelArr = np.load(label_path)
         if transpose:
             labelArr = np.transpose(labelArr).reshape(1, -1)
